d908a5bd0aa4809f8e456f0d4f70015f5bff8aeb
arakawa asuka <asuka@localhost.localdomain>
2024-08-25 14:17:12 +0900

    delete easy_gannt

--- openshift/redmine/plugins/easy_gantt/assets/javascripts/easy_gantt/jasmine/global_gantt.js
+++
@@ -1,8 +0,0 @@
-describeExtra("global_gantt", function () {
-  describe("FREE Global gantt", function () {
-    it("should fail anywhere but global gantt", function () {
-      expect(ysy.settings.global).toBe(true);
-      expect(ysy.settings.isGantt).toBe(true);
-    });
-  });
-});
--- openshift/redmine/plugins/easy_gantt/assets/javascripts/easy_gantt/jasmine/model_relations.js
+++
@@ -1,3 +0,0 @@
-describe("Model relations",function(){
-  
-});
